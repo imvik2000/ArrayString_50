1c842a37f6570843684cdf0a230dfba54490bd84
imvik2000 <71227014+imvik2000@users.noreply.github.com>
2021-05-21 21:49:34 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@
 Write your code in this editor and press "Run" button to execute it.
 
 *******************************************************************************/
-
+//kadane algo implemented to print max sum of contigous sub array....
 public class Main
 {
     public static void kadane(int[] arr)
